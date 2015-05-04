e36cb5b9934af119e3169c163e00da88aa983016
Doge <clement.kubec@isep.fr>
2015-05-04 17:52:04 +0200

    Merge origin/master
    Conflicts:
    	modele/add_ad.php

--- src/Environement/Environement.java
+++ src/Environement/Environement.java
@@ -8,6 +8,10 @@ package Environement;
 
 import IsepRampage.IsepRampage;
 import static IsepRampage.IsepRampage.window;
+import Object.ClusterShell;
+import Object.HeavyShell;
+import Object.NormalShell;
+import Object.SabotShell;
 import Object.Shell;
 import Object.Tank;
 import java.awt.Graphics;
@@ -38,6 +42,13 @@ public class Environement extends JComponent {
     public static int f = 0;
     public static boolean drawEnvironement = false;
     public static boolean setTank = false;
+    
+    private int colorShell;
+    private int fired;
+    private int typeShell;
+    private int setShell;
+    private int positionXShell;
+    private int positionYShell;
 
     public static Image BACKGROUND = Toolkit.getDefaultToolkit().createImage("nom.jpg");
 
@@ -115,7 +126,86 @@ public class Environement extends JComponent {
                 }
                
             
+               
+               //**********************************************
+               
+               
+               System.out.println("code touche = " + IsepRampage.keyPressed);
+        System.out.println("colorShell = " + colorShell);
+        System.out.println("colorShell = " + typeShell);
+        
+        
+        if (fired == 0){
+            if(IsepRampage.keyPressed == 49){           /*  touche "&"  */
+                typeShell = 1;
+                colorShell = 1;
+            }
+            else if(IsepRampage.keyPressed == 50){      /*  touche "é"  */
+                typeShell = 2;
+                colorShell = 2;
+            }
+            else if (IsepRampage.keyPressed == 51){     /*  touche """  */
+                typeShell = 3;
+                colorShell = 3;
+            }
+            else if (IsepRampage.keyPressed == 52){     /*  touche"'"   */
+                typeShell = 4;
+                colorShell = 4;
+            }
+
+            if(IsepRampage.keyPressed == 61 && (typeShell == 1 || typeShell == 2 || typeShell == 3 || typeShell == 4)){           /*  Touche "="  */
+                fired = 1;
+            }
+        }
+        
+        if (fired == 1){
             
+        System.out.println("setShell : " + setShell);
+            
+            if (setShell == 0){
+                positionXShell = positionXleftTank;
+                positionYShell = positionYleftTank;
+                setShell = 1;
+            }
+            else{
+            }
+            
+            if(typeShell == 1){ 
+                NormalShell launchedShell = new NormalShell(positionXShell, positionYShell);
+                
+                System.out.println("position en x : " + positionXShell);
+                System.out.println("position en y : " + positionYShell);
+                
+                launchedShell.move();
+                new DrawShell(g2d, launchedShell, positionXShell, positionYShell, colorShell);
+                positionXShell = launchedShell.positionSx;
+                positionYShell = launchedShell.positionSy;
+            }
+            else if (typeShell == 2){        
+                HeavyShell launchedShell = new HeavyShell(positionXleftTank, positionYleftTank);
+                launchedShell.move();
+                new DrawShell(g2d, launchedShell, launchedShell.positionSx, launchedShell.positionSy, colorShell);
+            }
+            else if (typeShell == 3){
+                ClusterShell launchedShell = new ClusterShell(positionXleftTank, positionYleftTank);
+                launchedShell.move();
+                new DrawShell(g2d, launchedShell, launchedShell.positionSx, launchedShell.positionSy, colorShell);
+            }
+            else if (typeShell == 4){
+                SabotShell launchedShell = new SabotShell(positionXleftTank, positionYleftTank);
+                launchedShell.move();
+                new DrawShell(g2d, launchedShell, launchedShell.positionSx, launchedShell.positionSy, colorShell);
+            }
+            
+            
+            
+        }
+        
+        
+        
+               
+               
+            //****************************************************
             if (positionXleftTank > 1250) {
             leftTank.positionX=1250;
         }
